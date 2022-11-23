1dfacd74d4511d9e40ab47d33f38a0e9afb6358a
mdahirr <m.dahir@hotmail.co.uk>
2022-11-23 16:02:46 +0000

    safariParkApp added

---
+++ SafariPark/SafariParkApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SafariParkApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person phil = new Person("phil", "Windridge", 28);
+            Console.WriteLine(phil.GetFullName());
+
+            //Person laura = new Person("Laura");
+            //Console.WriteLine(laura.GetFullName());
+
+            phil.Age = 54;
+            Console.WriteLine($"Phil is actually {phil.Age}");
+
+        }
+    }
+}
---
+++ SafariPark/SafariParkApp/person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafariParkApp
+{
+    internal class Person
+    {
+        private string _firstName = "";
+        private string _lastName = " ";
+
+
+
+        public Person(string firstName, string lastName, int age = 0)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            Age = age;
+        }
+
+
+
+        public int Age { get; set; }
+
+
+
+        public string GetFullName()
+        {
+            return $"{_firstName} {_lastName}";
+        }
+    }
+}
